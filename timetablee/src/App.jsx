c996371cb083c2d6a4df86547dd6a930d4fcbf36
Ibrahim Awoleiro <ibrahimawoleiro@Ibrahims-MBP.wifi.gsu.edu>
2023-09-20 16:13:00 -0400

    Saving project 1

---
+++ timetablee/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Calendar from './components/calendar.jsx'
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>TimeTable</h1>
+      <h2>This is a TimeTable</h2>
+      <Calendar></Calendar>
+    </div>
+  )
+}
+
+export default App
